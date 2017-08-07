fc05f3173d1a558adc4e14a907fd71b771d0d2da
Nate Balaoing <napahiba@gmail.com>
2017-08-07 22:30:47 +0000

    adventure game update

--- Logic/adventure_game.py
+++ Logic/adventure_game.py
@@ -145,7 +145,8 @@ def stairs1():
     sys.stdout.write('\n')
     sys.stdout.write('Finally, you reach the bottom of the stair, but you can\'t see a thing.')
     sys.stdout.write('\n')
-    next()
+    next_clear()
+    
     if game.weapon == 'amethyst wand':
         sys.stdout.write('You raise your wand and cast a spell')
         sys.stdout.write('\n')
@@ -164,7 +165,12 @@ def stairs1():
             if which_way == 'FORWARD':
                 proceed = True
                 sys.stdout.write('You carefully tread along the narrow path and continue through the door on the other side.')
-                return 'FORWARD'
+                sys.stdout.write('\n')
+                sys.stdout.write('Through the door, you enter the underground catacombs.')
+                sys.stdout.write('\n')
+                sys.stdout.write('You make your way through the catacombs and escape the dungeon.')
+                sys.stdout.write('\n')
+                sys.stdout.write('CONGRATULATIONS! You have escaped the dungeon.')
             elif which_way == 'BACK':
                 cell_hall()
             else:
@@ -175,8 +181,12 @@ def stairs1():
             which_way = raw_input('Will you proceed? FORWARD/BACK')
             if which_way == 'FORWARD':
                 proceed = True
-                sys.stdout.write('You carefully tread along the narrow path and continue through the door on the other side.')
-                return 'FORWARD'
+                sys.stdout.write('You make your way through the darkness, but, after just a few steps,')
+                sys.stdout.write('\n')
+                sys.stdout.write('the ground is gone from beneath your feet.')
+                sys.stdout.write('\n')
+                sys.stdout.write('You fall into a bottomless pit, never to be seen again.')
+                
             elif which_way == 'BACK':
                 proceed = True
                 cell_hall()
@@ -185,7 +195,99 @@ def stairs1():
         
 
 def door1():
-    sys.stdout.write('Door Room 1')
+    clear_screen()
+    sys.stdout.write('You make your way to the large wooden door.')
+    door_choice = False
+    while door_choice == False:
+        sys.stdout.write('\n')
+        which_way = raw_input('What will you do? OPEN or PEEK')
+        if which_way == 'OPEN':
+            door_choice = True
+            sys.stdout.write('\n')
+            sys.stdout.write('You wrench the door open with a loud creak, calling the attention of the sentry at watch.')
+            sys.stdout.write('\n')
+            sys.stdout.write('He promptly draws his sword and runs you through.')
+        elif which_way == 'PEEK':
+            door_choice = True
+            sys.stdout.write('\n')
+            sys.stdout.write('You gently inch the door open. Peering through the sliver of doorway,')
+            sys.stdout.write('\n')
+            sys.stdout.write('you see a single sentry standing watch, his back almost touching the door.')
+            if game.weapon == 'silver dagger':
+                sys.stdout.write('\n')
+                sys.stdout.write('With dagger in hand, you thrust it\'s sharp tip into his side.')
+                sys.stdout.write('\n')
+                sys.stdout.write('His body false limp, and you let his body slide to the floor.')
+                sys.stdout.write('\n')
+                next_clear()
+                sys.stdout.write('Keeping to the shadows, you and your companion stealthily make your way to the dungeon entrance.')
+                sys.stdout.write('\n')
+                sys.stdout.write('You slip out undetected.')
+                sys.stdout.write('\n' * 2)
+                sys.stdout.write('CONGRATULATIONS! You escaped the dungeon.')
+            elif game.weapon == 'oaken shield':
+                sys.stdout.write('\n')
+                sys.stdout.write('You wrench the door open with a loud creak.')
+                sys.stdout.write('\n')
+                sys.stdout.write('Before the guard realizes what is happening, you lunge with all your weight behind your shield.')
+                sys.stdout.write('\n')
+                sys.stdout.write('You both land in a pile on the floor. He\'s out cold, but you give him a sharp jab to the nose for good measure.')
+                next_clear()
+                sys.stdout.write('The ruckus of your attack has alerted the other guards. You hear them assembling in the next room.')
+                sys.stdout.write('\n')
+                sys.stdout.write('"Now you\'ve done it," you hear your companion grumble behind you.')
+                sys.stdout.write('\n')
+                sys.stdout.write('She draws her blade and readies for battle.')
+                sys.stdout.write('\n')
+                fight_choice = False
+                fight = raw_input('What will you do? DEFEND or ATTACK')
+                while fight_choice == False:
+                    if fight == 'DEFEND':
+                        fight_choice = True
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You upturn a table and brace the door. With your shield on guard, ')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('you await the skirmish about to erupt.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('The door bursts open with a fireball. The soldiers rush in.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You fight your hardest, but there are just too many of them.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You succumb to their military strength.')
+                    elif fight == "ATTACK":
+                        fight_choice = True
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You nod to your companion, waiting at the next door.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You give her a confident nod.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('She swings the door open, and you barrell through the door way at ramming speed.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You can hear the clashing of swords just at your back.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You hear the zip of loosed arrows.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You duck behind your shield and feel the thud as they make contact with you shield.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('There\'s no looking back.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You continue on at top speed, hurling your shield against oncoming soldiers.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You finally make it to the dungeon\'s entrance.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('You burst through with your comrade close at your heels.')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('\n')
+                        sys.stdout.write('CONGRATULATIONS! You have escaped the dungeon.')
+                        
+                        
+            elif game.weapon == 'amethyst wand':
+                sys.stdout.write('\n')
+                sys.stdout.write('Suddenly, the guard turns around. He promptly draws his sword and runs you through.')
+        else:
+            sys.stdout.write('\n')
+            sys.stdout.write('Please choose OPEN or PEEK.')
+        
 
 def game():
 
